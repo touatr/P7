774352a63dd60a679f01c95a7a9ed6489fc0def8
touatr <touati.ram@gmail.com>
2022-04-13 19:45:07 +0200

    Correction fonction filter

--- javascript/algo.js
+++ javascript/algo.js
@@ -9,7 +9,7 @@ searchBar.addEventListener('keyup', getSearchResult);
 //Fonction de callback de l'événement keyup
 function getSearchResult() {
     //Chercher dans les noms d'une recette les mots entrès dans la barre de recherche
-    const namesSuggestion = getSearchInNames();
+    const namesSuggestion = getSearchInNamesDescriptionIngredients();
     console.log(namesSuggestion);
     for( let i = 0; i < namesSuggestion.length; i++) {
         //Création de la carte recette
@@ -34,12 +34,6 @@ function getSearchResult() {
         ingredientsDescription.setAttribute('class', 'ingredients-description');
         const ingredients = document.createElement('div');
         ingredients.setAttribute('class', 'ingredients');
-        const ingredient = document.createElement('p');
-        ingredient.setAttribute('class', 'ingredient');
-        const quantity = document.createElement('p');
-        quantity.setAttribute('class', 'quantity');
-        const unit = document.createElement('p');
-        unit.setAttribute('class', 'unit');
         const description = document.createElement('p');
         description.setAttribute('class', 'description');
         recipesContainer.appendChild(recipeCard);
@@ -53,21 +47,30 @@ function getSearchResult() {
         timeRecipe.appendChild(time);
         ingredientsDescription.appendChild(ingredients);
         ingredientsDescription.appendChild(description);
-        ingredients.appendChild(ingredient);
-        ingredients.appendChild(quantity);
         title.innerHTML = namesSuggestion[i].name;
         time.innerHTML = namesSuggestion[i].time + " " + "min";
-        ingredient.innerHTML = namesSuggestion[i].ingredients[i].ingredient;
-        quantity.innerHTML = namesSuggestion[i].ingredients[i].quantity;
+        for(let j = 0; j < namesSuggestion[i].ingredients.length; j++) {
+            const ingredient = document.createElement('p');
+            ingredient.setAttribute('class', 'ingredient');
+            const quantity = document.createElement('p');
+            quantity.setAttribute('class', 'quantity');
+            let unit = document.createElement('p');
+            unit.setAttribute('class', 'unit');
+            ingredients.appendChild(ingredient);
+            ingredients.appendChild(quantity);
+            ingredients.appendChild(unit);
+            ingredient.innerHTML = namesSuggestion[i].ingredients[j].ingredient + ":";
+            quantity.innerHTML = namesSuggestion[i].ingredients[j].quantity;
+            unit.innerHTML = namesSuggestion[i].ingredients[j].unit;
+            //Tester les cas ou il n'ya pas d'unité
+            if(unit.innerHTML === "undefined") {
+                unit.innerHTML = "";
+            }
+        }
         description.innerHTML = namesSuggestion[i].description;
         recipeCard.appendChild(recipeHeader);
         recipeCard.appendChild(recipeText);
     }
-
-    //Chercher dans les descriptions d'une recette les mots entrès dans la barre de recherche
-    const descriptionsSuggestion = getSearchInDescription();
-    //Chercher dans les ingrédients d'une recette les mots entrès dans la barre de recherche
-    const ingredientsSuggestion = getSearchInIngredients();
 }
 
 //Afficher les recettes par rapport aux mots entrés
@@ -77,34 +80,25 @@ function displayRecipes() {
 
 /*Fonctions qui cherchent des mots dans les titres, les descriptions et les ingrédeints
 d'une recette*/
-function getSearchInNames() {
+function getSearchInNamesDescriptionIngredients() {
     const searchBarInput = searchBar.value;
     //Filtrer les données du tableau des recettes qui inclus les entrèes de notre input
     //totalLowerCase ne fait pas la différence entre miniscule et majuscule
     //Includes() permet de comparer les données et les entrèes input
-    const namesSuggestion = recipes.filter(item => item.name.toLocaleLowerCase()
-    .includes(searchBarInput.toLocaleLowerCase()));
+    const namesSuggestion = recipes.filter((item => item.name.toLocaleLowerCase()
+    .includes(searchBarInput.toLocaleLowerCase())) || (item => item.description.toLocaleLowerCase()
+    .includes(searchBarInput.toLocaleLowerCase())) || (item => item.ingredients.toLocaleLowerCase()
+    .includes(searchBarInput.toLocaleLowerCase())));
     return namesSuggestion;
-}
-
-function getSearchInDescription() {
-    const searchBarInput = searchBar.value;
-    const descriptionsSuggestion = recipes.filter(item => item.description.toLocaleLowerCase()
-    .includes(searchBarInput.toLocaleLowerCase()));
-    return descriptionsSuggestion;
 }
 
 function getSearchInIngredients() {
     const searchBarInput = searchBar.value;
-    const ingredientsSuggestion = [];
-    //Boucle for qui parcout tout le tableau recipes
-    for(let i = 0; i < recipes.length; i++) {
-        let ingredient = recipes[i].ingredients.filter(item => item.ingredient
-        .toLocaleLowerCase().includes(searchBarInput.toLocaleLowerCase()));
-        //Supprimer les tableaux d'ingrédients vides
-        if(ingredient.length > 0) {
-            ingredientsSuggestion.push(ingredient);
-        }
-    }
-    return ingredientsSuggestion;
+    let ingredient = recipes.filter(item => item.ingredients.filter(item => item.ingredient
+    .toLocaleLowerCase().includes(searchBarInput.toLocaleLowerCase()))
+    .toLocaleLowerCase().includes(searchBarInput.toLocaleLowerCase()));
+    return ingredient;
 }
+
+const test = getSearchInIngredients();
+console.log(test);
