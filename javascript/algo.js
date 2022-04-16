8506be33a44c9a1c0fdbe0aa7cb6600b8cd4c0ef
touatr <touati.ram@gmail.com>
2022-04-16 19:52:05 +0200

    Affichage des cartes des recettes

--- javascript/algo.js
+++ javascript/algo.js
@@ -6,12 +6,18 @@ const recipesContainer = document.querySelector('.recipes-container');
 searchBar.addEventListener('keyup', getSearchResult);
 
 
-//Fonction de callback de l'événement keyup
+//Fonction de callback de l'événement keyup qui récupére toutes les recettes filtrèes
 function getSearchResult() {
-    //Chercher dans les noms d'une recette les mots entrès dans la barre de recherche
-    const namesSuggestion = getSearchInNamesDescriptionIngredients();
-    console.log(namesSuggestion);
-    for( let i = 0; i < namesSuggestion.length; i++) {
+    //Chercher dans les noms ou ingredients ou description d'une recette
+    //les mots entrès dans la barre de recherche
+    const inputSuggestion = getSearchInNamesDescriptionIngredients();
+    console.log(inputSuggestion);
+    getCardRecipe(inputSuggestion);
+}
+
+//Création d'une carte de recette
+function getCardRecipe(inputSuggestion) {
+    for( let i = 0; i < inputSuggestion.length; i++) {
         //Création de la carte recette
         const recipeCard = document.createElement('div');
         recipeCard.setAttribute('class', 'recipe-card');
@@ -33,8 +39,8 @@ function getSearchResult() {
         time.setAttribute('class', 'time');
         const ingredientsDescription = document.createElement('div');
         ingredientsDescription.setAttribute('class', 'ingredients-description');
-        const ingredients = document.createElement('div');
-        ingredients.setAttribute('class', 'ingredients-bloc');
+        const ingredientsBloc = document.createElement('div');
+        ingredientsBloc.setAttribute('class', 'ingredients-bloc');
         const description = document.createElement('p');
         description.setAttribute('class', 'description');
         recipesContainer.appendChild(recipeCard);
@@ -46,11 +52,12 @@ function getSearchResult() {
         titleTime.appendChild(timeRecipe);
         timeRecipe.appendChild(timeIcon);
         timeRecipe.appendChild(time);
-        ingredientsDescription.appendChild(ingredients);
-        ingredientsDescription.appendChild(description);
-        title.innerHTML = namesSuggestion[i].name;
-        time.innerHTML = namesSuggestion[i].time + " " + "min";
-        for(let j = 0; j < namesSuggestion[i].ingredients.length; j++) {
+        title.innerHTML = inputSuggestion[i].name;
+        time.innerHTML = inputSuggestion[i].time + " " + "min";
+        //Création de l'élément ingrédients
+        for(let j = 0; j < inputSuggestion[i].ingredients.length; j++) {
+            const ingredients = document.createElement('div');
+            ingredients.setAttribute('class', 'ingredients-ingredient-quantity-unit');
             let ingredient = document.createElement('p');
             ingredient.setAttribute('class', 'ingredient');
             const quantityUnit = document.createElement('div');
@@ -59,28 +66,31 @@ function getSearchResult() {
             quantity.setAttribute('class', 'quantity');
             let unit = document.createElement('p');
             unit.setAttribute('class', 'unit');
+            ingredientsDescription.appendChild(ingredients);
+            ingredients.appendChild(ingredient);
             quantityUnit.appendChild(quantity)
             quantityUnit.appendChild(unit);
-            ingredients.appendChild(ingredient);
             ingredients.appendChild(quantityUnit);
-            ingredient.innerHTML = namesSuggestion[i].ingredients[j].ingredient + ":";
-            quantity.innerHTML = namesSuggestion[i].ingredients[j].quantity;
-            unit.innerHTML = namesSuggestion[i].ingredients[j].unit;
-            //Tester les cas ou il n'ya pas d'unité
-            unit.innerHTML = namesSuggestion[i].ingredients[j].unit;
+            ingredientsBloc.appendChild(ingredients);
+            ingredient.innerHTML = inputSuggestion[i].ingredients[j].ingredient + ":";
+            quantity.innerHTML = inputSuggestion[i].ingredients[j].quantity;
+            unit.innerHTML = inputSuggestion[i].ingredients[j].unit;
+            //Tester le cas ou il n'ya pas d'unité
              if(unit.innerHTML === "undefined") {
                 unit.innerHTML = "";
             }
+            //Tester le cas ou il n'ya pas de quantité
+            if(quantity.innerHTML === "undefined") {
+                quantity.innerHTML = "";
+                ingredient.innerHTML = inputSuggestion[i].ingredients[j].ingredient;
+            }
         }
-        description.innerHTML = namesSuggestion[i].description;
+        description.innerHTML = inputSuggestion[i].description;
+        ingredientsDescription.appendChild(ingredientsBloc);
+        ingredientsDescription.appendChild(description);
         recipeCard.appendChild(recipeHeader);
         recipeCard.appendChild(recipeText);
     }
-}
-
-//Afficher les recettes par rapport aux mots entrés
-function displayRecipes() {
-
 }
 
 /*Fonctions qui cherchent des mots dans les titres, les descriptions et les ingrédeints
@@ -90,11 +100,11 @@ function getSearchInNamesDescriptionIngredients() {
     //Filtrer les données du tableau des recettes qui inclus les entrèes de notre input
     //totalLowerCase ne fait pas la différence entre miniscule et majuscule
     //Includes() permet de comparer les données et les entrèes input
-    const namesSuggestion = recipes.filter((item => item.name.toLocaleLowerCase()
+    const inputSuggestion = recipes.filter((item => item.name.toLocaleLowerCase()
     .includes(searchBarInput.toLocaleLowerCase())) || (item => item.description.toLocaleLowerCase()
     .includes(searchBarInput.toLocaleLowerCase())) || (item => item.ingredients.toLocaleLowerCase()
     .includes(searchBarInput.toLocaleLowerCase())));
-    return namesSuggestion;
+    return inputSuggestion;
 }
 
 function getSearchInIngredients() {
@@ -103,6 +113,10 @@ function getSearchInIngredients() {
     .toLocaleLowerCase().includes(searchBarInput.toLocaleLowerCase()))
     .toLocaleLowerCase().includes(searchBarInput.toLocaleLowerCase()));
     return ingredient;
+}
+
+//Afficher les recettes par rapport aux mots entrés
+function displayRecipes(inputSuggestion) {
 }
 
 /*const test = getSearchInIngredients();
